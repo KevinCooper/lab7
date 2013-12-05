da1c670c545d05cedea121f032015e956516e1cc
Kevin Cooper <boo2atfoo@gmail.com>
2013-12-05 11:13:07 -0700

    Changed the coade to use the LED's to verify that the directions are working correctly.

--- main.c
+++ main.c
@@ -1,10 +1,32 @@
 #include <msp430.h> 
-
-/*
- * main.c
- */
+#include "robotLibrary/robotLibrary.h"
+#include "SensorLibrary/SensorLibrary.h"
+#define FALSE 0
+#define TRUE 1
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
-	
+
+    P1DIR |= BIT0;
+    P1DIR |= BIT6;
+    //initRobot();
+    //moveForward(30, 30);
+    for(;;){
+    	if(isCenterClose()==TRUE){
+    		P1OUT |= BIT0;
+    		P1OUT |= BIT6;
+    	}else{
+    		P1OUT &= ~BIT0;
+    		P1OUT &= ~BIT6;
+    	}
+    	//while(isCenterClose() == FALSE){
+    	//	moveForward(30, 30);
+        //	while(isRightClose() == FALSE){
+        //		halfRight();
+        //	}
+    	//}
+    	//leftTurn();
+    }
+
+
 	return 0;
 }
