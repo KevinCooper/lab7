6e6b12246d018abce901a049dfda540088a1b24b
Kevin Cooper <boo2atfoo@gmail.com>
2013-12-09 02:49:03 -0700

    Added code to test the library and verify that it meets the required functionality requirements.

--- main.c
+++ main.c
@@ -3,30 +3,24 @@
 #include "SensorLibrary/SensorLibrary.h"
 #define FALSE 0
 #define TRUE 1
-int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 
-    P1DIR |= BIT0;
-    P1DIR |= BIT6;
-    //initRobot();
-    //moveForward(30, 30);
-    for(;;){
-    	if(isCenterClose()==TRUE){
-    		P1OUT |= BIT0;
-    		P1OUT |= BIT6;
-    	}else{
-    		P1OUT &= ~BIT0;
-    		P1OUT &= ~BIT6;
-    	}
-    	//while(isCenterClose() == FALSE){
-    	//	moveForward(30, 30);
-        //	while(isRightClose() == FALSE){
-        //		halfRight();
-        //	}
-    	//}
-    	//leftTurn();
-    }
+	P1DIR |= (BIT0|BIT6);
 
+	for (;;) {
+		if (isLeftClose() == TRUE) {
+			P1OUT |= BIT0;
+		} else {
+			P1OUT &= ~BIT0;
+		}
+		if (isRightClose() == TRUE) {
+			P1OUT |= BIT6;
+		} else {
+			P1OUT &= ~BIT6;
+		}
+	}
 
 	return 0;
 }
